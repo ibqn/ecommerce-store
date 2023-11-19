fd2d0b01ef8b115e88e12bb4fa1003e35d9b1123
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-11-19 10:49:38 +0100

    fix type exports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
 export { type BillboardType } from './billboard'
 export { type CategoryType } from './category'
 export { type ProductType } from './product'
+export { type SizeType } from './size'
+export { type ColorType } from './color'
