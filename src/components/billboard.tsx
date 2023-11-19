7a7efb4243bf9194aff49d6dee92d7a14d7cfb09
ibqn <evgen.ibqn@gmail.com>
2023-11-19 19:22:32 +0100

    improve billboard

--- src/components/billboard.tsx
+++ src/components/billboard.tsx
@@ -1,4 +1,4 @@
-import { BillboardType } from '@/types'
+import { type BillboardType } from '@/types'
 import Image from 'next/image'
 
 type Props = {
@@ -10,18 +10,18 @@ export const Billboard = ({ billboard }: Props) => {
 
   return (
     <div className="relative m-4 flex h-[300px] overflow-hidden rounded-xl sm:m-6 lg:m-8">
-      <div className="flex h-full w-full flex-col items-center justify-center gap-y-8 text-center">
-        <div className="max-w-xs text-3xl font-bold sm:max-w-xl sm:text-5xl lg:text-6xl">
-          {label}
-        </div>
-      </div>
-
       <Image
         fill
         src={imageUrl}
         alt="Billboard"
-        className="-z-10 aspect-square overflow-hidden rounded-xl object-cover"
+        className="aspect-square overflow-hidden rounded-xl object-cover"
       />
+
+      <div className="z-10 flex h-full w-full flex-col items-center justify-center gap-y-8 text-center">
+        <div className="max-w-xs text-3xl font-bold sm:max-w-xl sm:text-5xl lg:text-6xl">
+          {label}
+        </div>
+      </div>
     </div>
   )
 }
