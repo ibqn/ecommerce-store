3ded17b340a1985b3d3aca19aa407e3e9407ce20
ibqn <evgen.ibqn@gmail.com>
2023-11-19 19:22:44 +0100

    export actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,8 @@
 export { getCategories } from './get-categories'
 export { getBillboards } from './get-billboards'
 export { getProducts } from './get-products'
+export { getSizes } from './get-sizes'
+export { getColors } from './get-colors'
 
 export { getProduct } from './get-product'
+export { getCategory } from './get-category'
