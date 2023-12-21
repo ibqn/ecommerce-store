2e5e26782efdf1ce71fc9b7136320d77f5ac413a
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-12-21 16:02:21 +0100

    show current year in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,15 @@
+import { useMemo } from 'react'
+
 type Props = {}
 
 export const Footer = (props: Props) => {
+  const currentYear = useMemo(() => new Date().getFullYear(), [])
+
   return (
     <footer className="border-t bg-white">
       <div className="mx-auto py-10">
         <p className="text-center text-sm text-black">
-          &copy; 2023 Store, Inc. All rights reserved.
+          &copy; {currentYear} Store, Inc. All rights reserved.
         </p>
       </div>
     </footer>
