29dd106dbfdca497d86e8cd802140b59321ce0d0
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-12-22 14:47:43 +0100

    show number of items in cart

--- src/components/navbar-actions.tsx
+++ src/components/navbar-actions.tsx
@@ -1,14 +1,19 @@
-import { Button } from '@/components/ui/button'
-import { ShoppingBag } from 'lucide-react'
+import { Button } from "@/components/ui/button"
+import { useCart } from "@/hooks/use-cart"
+import { ShoppingBag } from "lucide-react"
 
 type Props = {}
 
 export const NavbarActions = (props: Props) => {
+  const cart = useCart()
+
   return (
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center gap-x-2 rounded-full">
         <ShoppingBag size={20} color="white" />
-        <span className="text-sm font-medium text-white">0</span>
+        <span className="text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   )
