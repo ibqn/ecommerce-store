8ff0bc44152fbdcb4672c6938fe6ab4f08b3a910
Evgeny Bobkin <evgeny.bobkin@europace.de>
2023-12-29 11:03:53 +0100

    forward icon for icon button

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,13 +1,14 @@
 import { cn } from '@/utils'
-import { type ComponentProps, type ReactElement } from 'react'
+import { forwardRef, type ComponentProps, type ReactElement } from 'react'
 
 type Props = {
   icon: ReactElement
 } & ComponentProps<'button'>
 
-export const IconButton = ({ className, icon, ...props }: Props) => {
+export const IconButton = forwardRef<HTMLButtonElement, Props>(({ className, icon, ...props }, ref) => {
   return (
     <button
+      ref={ref}
       {...props}
       className={cn(
         'flex items-center justify-center rounded-full border bg-white p-2 shadow-md',
@@ -18,4 +19,6 @@ export const IconButton = ({ className, icon, ...props }: Props) => {
       {icon}
     </button>
   )
-}
+})
+
+IconButton.displayName = "IconButton"
